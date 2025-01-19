677ea8641c417b0cb8eb75d5dc23411c272c29e3
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2025-01-19 11:39:54 +0200

    fix empty liked page

--- src/pages/liked/Liked.tsx
+++ src/pages/liked/Liked.tsx
@@ -6,7 +6,7 @@ import { RootState } from '../../redux-store/store';
 const Liked: React.FC = () => {
    const likeCatList = useSelector((state: RootState) => state.cat.likedCat);
    return (
-      <div className="flex items-center flex-col p-5">
+      <div className="h-screen flex items-center flex-col p-5">
          <ul className="grid grid-cols-2 md:grid-cols-3 gap-4 p-5">
             {likeCatList.map((e) => {
                return <ItemList key={e.id} catItem={e} url={e.url} unLike={true} />;
